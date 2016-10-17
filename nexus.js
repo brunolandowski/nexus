2f8f449559d0883ba0d6ac6a58beff50273837dc
Bruno Landowski <bruno.landowski@gmail.com>
2016-10-17 17:48:35 +0200

    Update nexus.js

--- nexus.js
+++ nexus.js
@@ -15,44 +15,39 @@
       	var parametres = $.extend(defauts, options); // We extend the defaults parameters.
 
         return this.each(function() {
+		$(this).css('position', 'absolute'); // Set positioning to enable overlapping. 
+		var theid = $(this).attr('id'); // Get the 'id' of the canvas.
 			
-			$(this).css('position', 'absolute'); // Set positioning to enable overlapping. 
-			var theid = $(this).attr('id'); // Get the 'id' of the canvas.
-			
-			// Default parameters to variables.
-			var div_class = parametres.selector;
-            var line_width = parametres.lineHeight;
-            var line_color = parametres.lineColor;
-            var line_cap = parametres.lineCap;
-            var taille = $(div_class).length;
+		// Default parameters to variables.
+		var div_class = parametres.selector;
+            	var line_width = parametres.lineHeight;
+            	var line_color = parametres.lineColor;
+            	var line_cap = parametres.lineCap;
+            	var taille = $(div_class).length;
 
-			if (parametres.bringBack) {
+	if (parametres.bringBack) {
                 $(this).css('z-index', -9999);
-            }
+	}
 
-			function ironLink() { 
-                $(div_class).each(function(i, index) {
-                    if (i == taille - 1) { 
+	function ironLink() {
+		$(div_class).each(function(i, index) {
+			if (i == taille - 1) { 
 
-                    // Check for the last boxe.
-                    // Don't draw link between first and last element
-
-                    } else {
+                    	// Check for the last boxe.
+                    	// Don't draw link between first and last element
+			} else {
                         var outerHeight = parseInt($(div_class).css("margin"));  // Margins of the boxes as a variable.
-						var marg_h = outerHeight - 0; 
+			var marg_h = outerHeight - 0; 
                         var marg_w = outerHeight - 0;
-
-						var premier_height = ($(this).outerHeight() + marg_h);
+			var premier_height = ($(this).outerHeight() + marg_h);
                         var premier_width = ($(this).outerWidth() + marg_w);
-
-						var premier_right = (premier_width + $(this).position().left);
+			var premier_right = (premier_width + $(this).position().left);
                         var premier_bottom = (premier_height + $(this).position().top);
-						var deuxieme_left = (marg_w + $(this).nextAll().position().left);
+			var deuxieme_left = (marg_w + $(this).nextAll().position().left);
                         var deuxieme_top = (marg_h + $(this).nextAll().position().top);
-
-						var c = document.getElementById(theid);
-						var ctx = c.getContext("2d");
-						var anchorvar = parametres.anchor;
+			var c = document.getElementById(theid);
+			var ctx = c.getContext("2d");
+			var anchorvar = parametres.anchor;
                         var anchor_right = parseFloat(anchorvar);
 
                         if (parametres.lineDash) {
@@ -61,56 +56,52 @@
                             ctx.setLineDash([0, 0]); // Check if lineDash is false.
                         }
 
-						ctx.beginPath(); // Let's start the drawing.
-
-                        ctx.moveTo(premier_right - anchor_right, premier_bottom - anchor_right); 
+			ctx.beginPath(); // Let's start the drawing.
+			ctx.moveTo(premier_right - anchor_right, premier_bottom - anchor_right); 
                         ctx.lineTo(deuxieme_left + anchor_right, deuxieme_top + anchor_right);
-
                         ctx.lineWidth = line_width;
                         ctx.strokeStyle = line_color
                         ctx.lineCap = line_cap;
-						ctx.stroke(); // Close the path.
+			ctx.stroke(); // Close the path.
                     }
                 });
             }
 
-			var canvas = document.getElementById(theid),
-            context = canvas.getContext('2d');
+	var canvas = document.getElementById(theid),
+	   	context = canvas.getContext('2d');
 
-            // Resize the canvas to fill browser window dynamically.
-            window.addEventListener('resize', resizeCanvas, true);
+            	// Resize the canvas to fill browser window dynamically.
+		window.addEventListener('resize', resizeCanvas, true);
 
-            function resizeCanvas() {
-                var doc_height = $(document).height();
-                var doc_width = $(window).width();
-                canvas.width = doc_width;
-                canvas.height = doc_height;
-				drawStuff();
-            }
+            	function resizeCanvas() {
+                	var doc_height = $(document).height();
+                	var doc_width = $(window).width();
+                	canvas.width = doc_width;
+                	canvas.height = doc_height;
+			drawStuff();
+            	}
             
-            resizeCanvas();
+            	resizeCanvas();
 
-            function drawStuff() {
-            }
+            	function drawStuff() {
+            	}
 
-			var height = $(window).height();
-            var width = $(window).width();
+		var height = $(window).height();
+            	var width = $(window).width();
 
-            ironLink();
+            	ironLink();
 
-            // Resize the canvas to fill browser window dynamically is window is resized.
-			$(window).resize(function() {
-				canvas.width = window.innerWidth;
-                canvas.height = window.innerHeight;
-				resizeCanvas();
-				ironLink();
-			});
-
-			
-			console.log("%cNexus plugin made by Bruno Landowski", "background: #333; color: #bdbd0a; padding: 5px 10px;");
-			console.log("%chttp://brunolandowski.fr", "color:#333");
-			console.log("%chttps://github.com/brunolandowski", "color:#333");
+            	// Resize the canvas to fill browser window dynamically is window is resized.
+		$(window).resize(function() {
+			canvas.width = window.innerWidth;
+                	canvas.height = window.innerHeight;
+			resizeCanvas();
+			ironLink();
+		});
 		
+		console.log("%cNexus plugin made by Bruno Landowski", "background: #333; color: #bdbd0a; padding: 5px 10px;");
+		console.log("%chttp://brunolandowski.fr", "color:#333");
+		console.log("%chttps://github.com/brunolandowski", "color:#333");
 		});
     };
 })(jQuery);
